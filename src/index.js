361c61f1c636848843204447473795a54a09910e
amartya-rs <saha.amartya@outlook.com>
2022-01-10 14:12:08 +0530

    refactor: update ES6+ syntax

--- src/index.js
+++ src/index.js
@@ -1,30 +1,31 @@
-var button = document.querySelector(".check-btn");
-var dob = document.querySelector(".input-dob");
-var luckyNo = document.querySelector(".input-lucky-no");
-var output = document.querySelector(".output-div");
+const button = document.querySelector(".check-btn");
+const dob = document.querySelector(".input-dob");
+const luckyNo = document.querySelector(".input-lucky-no");
+const output = document.querySelector(".output-div");
 
-//adding clcik event to button
-button.addEventListener("click", clickHandler);
 
 //click handler
-function clickHandler() {
+let clickHandler = () => {
   if (luckyNo.value !== "" && dob.value !== "") {
     //removing hyphen from the string
-    var dobString = dob.value.replaceAll("-", "");
+    const dobString = dob.value.replaceAll("-", "");
 
     //calculating sum of the digits of dob
-    var dobSum = 0;
-    for (var a = 0; a < dobString.length; a++) {
+    let dobSum = 0;
+    for (let a = 0; a < dobString.length; a++) {
       dobSum = dobSum + Number(dobString.charAt(a));
     }
 
     //checking whether sum of the digits of dob is divisible by lucky number
     if (dobSum % luckyNo.value === 0) {
-      output.innerText = luckyNo.value + " is a lucky number ^_^";
+      output.innerText = `${luckyNo.value} is a lucky number ^_^`;
     } else {
-      output.innerText = "Sorry," + luckyNo.value + " is not so lucky for you";
+      output.innerText = `Sorry, ${luckyNo.value} is not so lucky for you`;
     }
   } else {
-    alert("Enter both the values to get result");
+    alert('Enter both the values to get result');
   }
 }
+
+//adding clcik event to button
+button.addEventListener("click", clickHandler);
